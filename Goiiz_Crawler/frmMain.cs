30411a2fbc72e19c836b9a08225a2e1708103bae
knowlet <knowlet3389@gmail.com>
2015-09-05 17:21:01 +0800

    tweaks

--- Goiiz_Crawler/frmMain.cs
+++ Goiiz_Crawler/frmMain.cs
@@ -7,13 +7,17 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Goiiz_Crawler
 {
     public partial class frmMain : Form
     {
+        string PATH = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\Documents\\";
+        Regex regPCStore = new Regex(@"^https?://www\.pcstore\.com\.tw/(\w+)");
+        Regex regRakuten = new Regex(@"^https?://www\.rakuten\.com\.tw/shop/(\w+)");
+        Regex regYahooMall = new Regex(@"^https?://tw\.mall\.yahoo\.com/store/(\w+)");
+
         public frmMain()
         {
             InitializeComponent();
@@ -22,9 +26,6 @@ namespace Goiiz_Crawler
         private void btnStart_Click(object sender, EventArgs e)
         {
             string url = txtUrl.Text;
-            Regex regPCStore = new Regex(@"^https?://www\.pcstore\.com\.tw/(\w+)");
-            Regex regRakuten = new Regex(@"^https?://www\.rakuten\.com\.tw/shop/(\w+)");
-            Regex regYahooMall = new Regex(@"^https?://tw\.mall\.yahoo\.com/store/(\w+)");
             pgbShow.Value = 0;
             if (regPCStore.IsMatch(url))
             {
@@ -37,7 +38,7 @@ namespace Goiiz_Crawler
                     txtShow.AppendText(String.Format("找到 {0} 個產品，共 {1} 頁\n", bot.itemNum, bot.pageNum));
                     List<string> urls = bot.getItemUrlsList();
                     txtShow.AppendText("物品清單下載完成，開始取得物品資料");
-                    string path = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\" + id + ".csv";
+                    string path = PATH + id + ".csv";
                     File.Delete(path);
                     pgbShow.Maximum = bot.itemNum;
                     pgbShow.Minimum = 0;
@@ -66,7 +67,7 @@ namespace Goiiz_Crawler
                     // txtShow.AppendText(bot.getSinglePage("http://www.rakuten.com.tw/shop/pcgoex/product/100000010481842/?l-id=tw_search_product_1"));
                     List<string> urls = bot.getItemUrlsList();
                     txtShow.AppendText("物品清單下載完成，開始取得物品資料");
-                    string path = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\" + id + ".csv";
+                    string path = PATH + id + ".csv";
                     File.Delete(path);
                     pgbShow.Maximum = Convert.ToInt32(bot.itemNum);
                     pgbShow.Minimum = 0;
@@ -95,7 +96,7 @@ namespace Goiiz_Crawler
                     // txtShow.AppendText(bot.getSinglePage("https://tw.mall.yahoo.com/item/%E7%91%B0%E7%8F%80%E7%BF%A0-2015-%E5%8E%9F%E5%BB%A0%E8%AD%B7%E6%89%8B%E9%9C%9C%E4%B8%89%E5%85%A5%E7%A6%AE%E7%9B%92-p034075665212"));
                     List<string> urls = bot.getItemUrlsList();
                     txtShow.AppendText("物品清單下載完成，開始取得物品資料");
-                    string path = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\" + id + ".csv";
+                    string path = PATH + id + ".csv";
                     File.Delete(path);
                     pgbShow.Maximum = Convert.ToInt32(bot.itemNum);
                     pgbShow.Minimum = 0;
